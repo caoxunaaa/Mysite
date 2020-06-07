a8269e97cd868722fed40b9f66fcea027c2f6cc9
caoxunaaa <451501060@qq.com>
2020-06-08 00:12:50 +0800

    自学独立写博客网站。8th day.    ajax异步提交 part 1.

--- comment/views.py
+++ comment/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render, redirect
 from .forms import CommentForm
 from .models import Comment
 from django.urls import reverse
+from django.http import JsonResponse
 
 
 def update_comment(request):
     referer = request.META.get('HTTP_REFERER', reverse('home'))
     comment_form = CommentForm(request.POST, user=request.user)
+    data = dict()
     if comment_form.is_valid():
         comment = Comment()
         comment.user = comment_form.cleaned_data['user']
         comment.content_object = comment_form.cleaned_data['content_object']
         comment.context = comment_form.cleaned_data['text']
         comment.save()
-        return redirect(referer)
+        # 返回数据
+        data['status'] = 'SUCCESS'
     else:
-        return render(request, 'error.html', {'message': comment_form.errors})
+        # return render(request, 'error.html', {'message': comment_form.errors, 'redirect_to': referer})
+        data['status'] = 'ERROR'
+    return JsonResponse(data)
