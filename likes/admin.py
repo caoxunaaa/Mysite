38553a19ffcc3899e51a08234e35a22f9f656116
caoxunaaa <451501060@qq.com>
2020-06-10 17:31:29 +0800

    自学独立写博客网站。11th day.    点赞实现异步功能 part 1.
    Signed-off-by: caoxunaaa <451501060@qq.com>

--- likes/admin.py
+++ likes/admin.py
@@ -5,3 +5,8 @@ from .models import LikesCount, LikesRecord
 @admin.register(LikesCount)
 class LikesCountAdmin(admin.ModelAdmin):
     list_display = ('id', 'content_object', 'likes_number')
+
+
+@admin.register(LikesRecord)
+class LikesRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content_object', 'likes_time', 'likes_user')
--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import LikesCount, LikesRecord
+from django.contrib.contenttypes.models import ContentType
 
 # Create your views here.
-def like_change():
-    pass
+def like_change(request):
+    content_type = request.GET.get('contenttype')
+    object_id = request.GET.get('object_id')
+    print(content_type, object_id)
+    # ct = ContentType.objects.get(model=content_type)
+    # likescount, created = LikesCount.objects.get_or_create(content_type=ct, )
