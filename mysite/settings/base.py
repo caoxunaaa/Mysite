287e95f9e362df7077fe7ff8066e156a9d3ee9d1
caoxunaaa <451501060@qq.com>
2020-07-03 10:22:50 +0800

    添加静态文件收集

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -123,7 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'blog/blog/static'),
 ]
 
 # media
