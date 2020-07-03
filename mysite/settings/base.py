efa487c387887eeb376cf1fd59c9fc680f1ce85c
caoxunaaa <451501060@qq.com>
2020-07-03 10:12:38 +0800

    添加静态文件收集

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'blog/blog/static'),
